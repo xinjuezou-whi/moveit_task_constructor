c017f072b6a388dc9488c082a7e38f5563b20395
Xinjue Zou <hit_zou@126.com>
2023-02-15 15:43:33 +0800

    remove getErrorCodeString(result.error_code)

--- capabilities/src/execute_task_solution_capability.cpp
+++ capabilities/src/execute_task_solution_capability.cpp
@@ -109,11 +109,11 @@ void ExecuteTaskSolutionCapability::goalCallback(
 		result.error_code = context_->plan_execution_->executeAndMonitor(plan);
 	}
 
-#if HAVE_MOVEIT_ERROR_CODE_TO_STRING
+//#if HAVE_MOVEIT_ERROR_CODE_TO_STRING
 	const std::string response = moveit::core::MoveItErrorCode::toString(result.error_code);
-#else
-	const std::string response = context_->plan_execution_->getErrorCodeString(result.error_code);
-#endif
+//#else
+//	const std::string response = context_->plan_execution_->getErrorCodeString(result.error_code);
+//#endif
 
 	if (result.error_code.val == moveit_msgs::MoveItErrorCodes::SUCCESS)
 		as_->setSucceeded(result, response);
